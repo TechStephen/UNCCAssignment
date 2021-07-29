1c5d57062bc3d38117f25f1657f05b2d0cc664d2
TechStephen <68354435+TechStephen@users.noreply.github.com>
2021-07-29 08:29:48 -0400

    Initial Commit
    Numbers.txt needed to run

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace evensandodds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Stephen\source\repos\evensandodds\evensandodds\Numbers.txt");
+            int[] arr = new int[lines.Length];
+
+            for(int i = 0; i < lines.Length; i++)
+            {
+                arr[i] = Int16.Parse(lines[i]);
+                isEven(arr[i]);
+            }
+        }
+
+        static void isEven(int num)
+        {
+            if(num % 2 == 0)
+            {
+                Console.WriteLine(num + " is even");
+            }
+            else
+            {
+                Console.WriteLine(num + " is odd");
+            }
+        }
+    }
+}
